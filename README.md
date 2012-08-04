0db4ef856b528f51bd05175fef1bfc906f3b683b
Michal Papis <mpapis@gmail.com>
2012-08-05 01:05:35 +0200

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# SMF Extension for testing
+
+Allow running the same tests in multiple virtual machines.
+
+## Vagrant
+
+### Generate Vagrantfile
+
+
+    sm sm_tester generate config <box_name> <box_url> <target_file> [param=value ...]
---
+++ shell/vagrant/vagrant_comment_test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+eval $(sm test ext)
+includes sm_tester/vagrant
+
+mkdir -p "${sm_temp_path}/sm_tester/"
+[[ -f "${sm_temp_path}/sm_tester/Vagrantfile" ]] # status!=0
+
+generate_vm_config ubuntu_1204_64_precise http://files.vagrantup.com/precise64.box "${sm_temp_path}/sm_tester/Vagrantfile" cpus=4 memory=4096
+# status=0
+
+[[ -f "${sm_temp_path}/sm_tester/Vagrantfile" ]] # status=0
+
+cat "${sm_temp_path}/sm_tester/Vagrantfile"
+# match=/config.vm.box     = "ubuntu_1204_64_precise"/
+# match=/config.vm.box_url = "http://files.vagrantup.com/precise64.box"/
